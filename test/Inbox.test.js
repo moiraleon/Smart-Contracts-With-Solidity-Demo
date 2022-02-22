300635cd098ad52bbf4b7031beb56b6707998ac2
Moira Leon <moira.leon2@gmail.com>
2022-02-21 21:59:59 -0700

    creating local instance of Web3

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,6 +3,6 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3'); //this is a constructor - we are making instances of the Web3 library - when utilizing constructors we capitalize
-
+console web3 = new Web3(ganache.provider());
 
 // contract test code will go here
