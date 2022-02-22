c732730e7333ae72413ee57c715b7359312e4423
Moira Leon <moira.leon2@gmail.com>
2022-02-21 18:16:15 -0700

    compile script

--- compile.js
+++ compile.js
@@ -1,3 +1,4 @@
+//first run: npm install solc@0.4.17 
 const path = require('path');
 const fs = require('fs');
 const solc = require('solc')
@@ -6,6 +7,8 @@ const inboxPath = path.resolve(__dirname,'contracts','Inbox.sol');
 const source = fs.readFileSync(inboxPath,'utf8');
 
 // compile code will go here
-solc.compile(source, 1)//pass in our source code to be compiled, and specify number of different contracts we are attempting to compile
+//solc.compile(source, 1)//pass in our source code to be compiled, and specify number of different contracts we are attempting to compile
 
-console.log(solc.compile(source, 1))//testing to see what compiler is doing
+//console.log(solc.compile(source, 1))//testing to see what compiler is doing
+
+module.exports = solc.compile(source, 1).contracts[':Inbox']; //gives us direct access to the object that contains just the bytecode
