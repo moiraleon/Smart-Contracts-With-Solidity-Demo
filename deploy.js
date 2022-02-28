f8e6672492a5de3d6fcb37a492d4491e5c69330d
Moira Leon <moira.leon2@gmail.com>
2022-02-28 14:44:43 -0700

    wallet provider setup

--- deploy.js
+++ deploy.js
@@ -1,1 +1,10 @@
-// deploy code will go here
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const {interface, bytecode} = require('./compile');
+
+const provider = new HDWalletProvider(
+    'make asset mechanic private picture message penalty language swift wolf virus staff',
+    'https://rinkeby.infura.io/v3/6c1009cb8115459680f9055ddcdeded0'
+    );
+
+    const web3 = new web3(provider);
